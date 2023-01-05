fe22c1a652859be911bda1b8220c3b2b268ea86f
Ramazanzade <ramazanov570633@gmail.com>
2023-01-05 04:43:06 +0300

    first commt

---
+++ App.js
@@ -0,0 +1,14 @@
+import Srack1 from './Navigator/Srack1';
+import TapBar from './Navigator/TapBar';
+export default function App() {
+  return <>
+<Srack1/>
+{/* <TapBar></TapBar>  */}
+
+
+
+
+ </>;
+}
+
+
